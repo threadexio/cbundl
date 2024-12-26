50fbf31483c7bb0e698970f9682763f58e465d87
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-12-26 19:14:19 +0200

    its bugged

--- test/minify/clean.c
+++ test/minify/clean.c
@@ -8,15 +8,14 @@
  *              / _| | '_ \| || || ' \))/ _` || |
  *              \__| |_.__/ \_,_||_||_| \__,_||_|
  *
- *                cbundl 0.1.2-debug (f260339+)
+ *                cbundl 0.1.2-debug (3bf8815+)
  *             https://github.com/threadexio/cbundl
  *
- *      Generated at: Thu 26 Dec 2024 18:37:28 (UTC+02:00)
+ *      Generated at: Thu 26 Dec 2024 19:13:37 (UTC+02:00)
  *
  *
- * You'd think the rain would've cooled things down.
- * All it did was make the heat wet.
- *   - Rear Window
+ * They say that all beauty must die, I say it just moves on.
+ *   - Avenged Sevenfold
  *
  */
 
@@ -27,9 +26,9 @@
 #include <stdio.h>
 int main() {
   int v1 = 5;
-  printf("Value of x: %d\n", v0);
-  for (int v3 = 0; v2 < 3; v2++) {
-    printf("This is loop iteration: %d\n", v2);
+  printf("Value of x: %d\n", v1);
+  for (int v3 = 0; v3 < 3; v3++) {
+    printf("This is loop iteration: %d\n", v3);
   }
   return 0;
 }
